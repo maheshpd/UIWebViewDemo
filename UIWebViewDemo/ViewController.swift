18e04c0394313b8837ee92f6a42aa97e3eaef56a
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-09-23 12:14:48 +0530

    Done

--- UIWebViewDemo/ViewController.swift
+++ UIWebViewDemo/ViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKUIDelegate,WKNavigationDelegate,WKScriptMessageHandler {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        
+        let webConfiguration = WKWebViewConfiguration()
+        let contentController = WKUserContentController()
+        //Inject JavaScript which sending message to App
+        let js: String = "window.webkit.messageHandlers.callbackHandler.postMessage('Hello from JavaScript');"
+        
+        let userScript = WKUserScript(source: js, injectionTime: WKUserScriptInjectionTime.atDocumentEnd, forMainFrameOnly: false)
+        
+        contentController.removeAllUserScripts()
+        contentController.addUserScript(userScript)
+        //Add ScriptMessageHandler
+        contentController.add(
+        self,
+        name: "callbackHandler"
+        )
+        
+        webConfiguration.userContentController = contentController
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let myURL = URL(string: "https://app.toolsninja.in/member")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
     }
 
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        if(message.name == "callbackHandler") {
+            print("JavaScript is sending a message \(message.body)")
+        }
+    }
 
 }
 
